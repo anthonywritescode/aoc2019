0018e2aac1a1804799ed0ef80534e01425ff835a
Anthony Sottile <asottile@umich.edu>
2019-12-08 00:04:45 -0800

    Implement day 8 part 2 in lua

---
+++ day08/part2.lua
@@ -0,0 +1,23 @@
+if #arg~=1 then
+    print(string.format("usage: %s FILENAME", arg[0]))
+    os.exit(1)
+end
+
+local fh = assert(io.open(arg[1], "r"))
+local contents = assert(fh:read("a"))
+fh:close()
+
+for y=0,5 do
+    for x=0,24 do
+        for layer=0,99 do
+            local i = 1 + (6 * 25 * layer) + (25 * y) + x
+            local c = contents:sub(i,i)
+            if c~="2" then
+                c = c:gsub("0", " ")
+                io.write(c)
+                break
+            end
+        end
+    end
+    io.write("\n")
+end

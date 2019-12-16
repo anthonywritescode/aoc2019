416378eff08d6ea4b537cb9e896964a423b10248
Anthony Sottile <asottile@umich.edu>
2019-12-16 10:56:34 -0800

    improve day 16 part 2

--- day16/part2.py
+++ day16/part2.py
@@ -1,43 +1,23 @@
 import argparse
-import itertools
-from typing import Generator
-from typing import Sequence
 
 import pytest
 
 from support import timing
 
 
-PATTERN = (0, 1, 0, -1)
-
-
-def repeat(pattern: Sequence[int], n: int) -> Generator[int, None, None]:
-    for val in pattern:
-        for _ in range(n):
-            yield val
-
-
 def compute(s: str) -> str:
     ints = [int(c) for c in s.strip()] * 10000
     offset = int(s[:7])
+    assert offset > len(ints) // 2, (offset, len(ints))
+    ints = ints[offset:]
 
     for iteration in range(100):
-        sums = [ints[-1]]
-        for num in reversed(ints[offset:-1]):
-            sums.append(sums[-1] + num)
-        sums = [0] * offset + list(reversed(sums))
+        restsum = sum(ints)
+        for i, n in enumerate(ints):
+            ints[i] = abs(restsum) % 10
+            restsum -= n
 
-        next_lst = ints[:offset]
-        for idx in range(offset, (len(ints) + 1) // 2):
-            pattern = itertools.cycle(repeat(PATTERN, idx + 1))
-            val = sum(x * y for x, y in zip(ints[idx:], pattern))
-            next_lst.append(abs(val) % 10)
-        for idx in range(max(offset, (len(ints) + 1) // 2), len(ints)):
-            next_lst.append(sums[idx] % 10)
-        assert len(ints) == len(next_lst)
-        ints = next_lst
-
-    return ''.join(str(n) for n in ints[offset:offset + 8])
+    return ''.join(str(n) for n in ints[:8])
 
 
 @pytest.mark.parametrize(

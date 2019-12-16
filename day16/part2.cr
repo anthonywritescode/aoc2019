00e431be60fe5b07bd4e163851120e2d2740e300
Anthony Sottile <asottile@umich.edu>
2019-12-16 11:12:56 -0800

    Improve part2 in crystal more

--- day16/part2.cr
+++ day16/part2.cr
@@ -6,14 +6,14 @@ end
 contents = File.read(ARGV[0]).strip
 ints = contents.chars.map(&.to_i) * 10000
 offset = contents[0...7].to_i
+ints = ints[offset..]
 
 100.times do
-  partial = ints[offset..-1].sum
-  (offset...ints.size).to_a.each do |e|
-    numb = partial
-    partial -= ints[e]
-    ints[e] = numb.abs % 10
+  restsum = ints.sum
+  ints.each_with_index do |num,i|
+    ints[i] = restsum.abs % 10
+    restsum -= num
   end
 end
 
-puts ints[offset...offset+8].join("")
+puts ints[...8].join("")
